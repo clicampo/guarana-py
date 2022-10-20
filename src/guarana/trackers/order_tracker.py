320000885cacb65909b07ab95c138ef59aa131e8
Gabriel Montañola <gabriel@montanola.com>
2022-10-20 16:03:14 -0300

    feat: add moar fields

--- src/guarana/trackers/order_tracker.py
+++ src/guarana/trackers/order_tracker.py
@@ -21,19 +21,33 @@ class OrderTracker(guarana.EventTracker):
             case "055ebaa9-7583-4d47-9ce8-a5d772f7a91d":
                 # Sampling order
                 identify_payload |= {
-                    "last_sampling_placed_ts": order_call.created_at,
-                    "last_sampling_placed_id": str(order_call.id_order),
+                    "last_sampling_order_placed_ts": order_call.created_at,
+                    "last_sampling_order_placed_id": str(order_call.id_order),
+                    "last_sampling_order_placed_total": order_call.total,
                 }
-            case _:
-                # Standard and return order
+            case "44895221-7870-4aeb-b570-4ceb251e9663":
+                # Standard order
                 identify_payload |= {
-                    "last_order_placed_ts": order_call.created_at,
-                    "last_order_placed_id": str(order_call.id_order),
-                    "last_order_placed_total": order_call.total,
+                    "last_standard_order_placed_ts": order_call.created_at,
+                    "last_standard_order_placed_id": str(order_call.id_order),
+                    "last_standard_order_placed_total": order_call.total,
+                }
+            case "c3ebe8d1-fde0-4a62-af3e-2e0ce5642408":
+                # Return order
+                identify_payload |= {
+                    "last_return_order_placed_ts": order_call.created_at,
+                    "last_return_order_placed_id": str(order_call.id_order),
+                    "last_return_order_placed_total": order_call.total,
                 }
 
+        identify_payload |= {
+            "last_order_placed_ts": order_call.created_at,
+            "last_order_placed_id": str(order_call.id_order),
+            "last_order_placed_total": order_call.total,
+        }
+
         if order_call.id_coupon:
-            identify_payload |= {"last_coupon_used": str(order_call.id_coupon)}
+            identify_payload |= {"last_coupon_used_id": str(order_call.id_coupon)}
 
         self.segment_client.identify(
             user_id=str(order_call.user_id),
